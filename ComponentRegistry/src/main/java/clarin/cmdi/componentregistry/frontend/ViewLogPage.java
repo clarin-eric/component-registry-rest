1e4b0053249065e271cd02a0ee5fb7bcdb277933
Twan Goosen <twan@clarin.eu>
2021-12-14 11:13:12 +0100

    Log4J upgraded to 2.16.0. Several other dependencies bumped.

--- ComponentRegistry/src/main/java/clarin/cmdi/componentregistry/frontend/ViewLogPage.java
+++ ComponentRegistry/src/main/java/clarin/cmdi/componentregistry/frontend/ViewLogPage.java
@@ -5,8 +5,11 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.text.NumberFormat;
-import org.apache.log4j.FileAppender;
-import org.apache.log4j.Logger;
+import java.util.Objects;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Appender;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.RollingFileAppender;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.link.DownloadLink;
@@ -20,55 +23,60 @@ import org.slf4j.LoggerFactory;
  * @author paucas
  */
 public class ViewLogPage extends SecureAdminWebPage {
-
+    
     private final static org.slf4j.Logger logger = LoggerFactory.getLogger(ViewLogPage.class);
-
+    
     public ViewLogPage(final PageParameters pageParameters) {
         super(pageParameters);
         addLinks();
         addLogFileContent();
     }
-
+    
     private void addLogFileContent() {
         final int tailSize = 1000000; // Megabyte
 
         final NumberFormat numberFormat = NumberFormat.getIntegerInstance();
         numberFormat.setGroupingUsed(true);
-
-        final File logFile = getLogFile();
-        if (logFile == null) {
-            addErrorValues("Log file not found");
-        } else {
-            try {
-                RandomAccessFile raLogFile = null;
+        
+        try {
+            final File logFile = getLogFile();
+            if (logFile == null) {
+                addErrorValues("Log file not found");
+            } else {
                 try {
-                    raLogFile = geFileTail(logFile, tailSize);
-                    final String content = getLogFileContent(raLogFile);
-
-                    add(new Label("logLabel", String.format("Showing final %s bytes (or less) of total %s in %s:", numberFormat.format(tailSize), numberFormat.format(raLogFile.length()), logFile)));
-                    add(new TextArea("logText", new Model(content)));
-
-                    add(new DownloadLink("logDownloadLink", logFile));
-                } catch (IOException ioEx) {
-                    addErrorValues(ioEx.getMessage());
-                    throw (ioEx);
-                } finally {
-                    if (raLogFile != null) {
-                        raLogFile.close();
+                    RandomAccessFile raLogFile = null;
+                    try {
+                        raLogFile = geFileTail(logFile, tailSize);
+                        final String content = getLogFileContent(raLogFile);
+                        
+                        add(new Label("logLabel", String.format("Showing final %s bytes (or less) of total %s in %s:", numberFormat.format(tailSize), numberFormat.format(raLogFile.length()), logFile)));
+                        add(new TextArea("logText", new Model(content)));
+                        
+                        add(new DownloadLink("logDownloadLink", logFile));
+                    } catch (IOException ioEx) {
+                        addErrorValues(ioEx.getMessage());
+                        throw (ioEx);
+                    } finally {
+                        if (raLogFile != null) {
+                            raLogFile.close();
+                        }
                     }
+                } catch (IOException ioEx) {
+                    logger.error("Error in reading log file", ioEx);
+                    throw ioEx;
                 }
-            } catch (IOException ioEx) {
-                logger.error("Error in reading log file", ioEx);
             }
+        } catch (Exception ex) {
+            addErrorValues(ex.getMessage());
         }
     }
-
+    
     private void addErrorValues(String message) {
         add(new Label("logLabel", "Could not read from log file. See error message below."));
         add(new TextArea("logText", new Model(message)));
         add(new DownloadLink("logDownloadLink", new File("")).setEnabled(false));
     }
-
+    
     private String getLogFileContent(final RandomAccessFile randomAccessFile) throws IOException {
         String currentLine;
         StringBuilder contentBuilder = new StringBuilder();
@@ -78,7 +86,7 @@ public class ViewLogPage extends SecureAdminWebPage {
         String content = contentBuilder.toString();
         return content;
     }
-
+    
     private RandomAccessFile geFileTail(File logFile, int tailLength) throws IOException, FileNotFoundException {
         // Skip to tail of file
         final RandomAccessFile raLogFile = new RandomAccessFile(logFile, "r");
@@ -90,13 +98,21 @@ public class ViewLogPage extends SecureAdminWebPage {
         }
         return raLogFile;
     }
-
-    private File getLogFile() {
+    
+    private File getLogFile() throws Exception {
         // Get file from appender
-        final FileAppender appender = (FileAppender) Logger.getRootLogger().getAppender("FILE");
-        if (appender != null) {
-            return new File(appender.getFile());
+        final LoggerContext context = (org.apache.logging.log4j.core.LoggerContext) LogManager.getContext();
+        Appender appender = context.getConfiguration().getAppender("FILE");
+        if (appender instanceof RollingFileAppender) {
+            final File file = new File(((RollingFileAppender) appender).getFileName());
+            if (file.exists()) {
+                return file;
+            } else {
+                throw new Exception("File configured in appender does not exist: " + file.getAbsolutePath());
+            }
+        } else {
+            logger.warn("Log view: unexpected appender {}, instance of RollingFileAppender expected", Objects.toString(appender));
+            throw new Exception("Cannot find log appender; found " + Objects.toString(appender) + ", expected RollingFileAppender");
         }
-        return null;
     }
 }
